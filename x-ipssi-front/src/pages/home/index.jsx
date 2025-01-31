cbef74193579152c175a4a0798940935dc4ea099
KiddMiguel <kidimbamiguel@gmail.com>
2025-01-31 01:22:07 +0100

    jhg

---
+++ x-ipssi-front/src/pages/home/index.jsx
@@ -0,0 +1,79 @@
+import PostForm from '../../components/PostForm/PostForm';
+import PostCard from '../../components/PostCard/PostCard';
+import SidebarRight from '../../components/SidebarRight/SidebarRight';
+import { getPostsBefore, clearStatus } from '../../redux/post/postSlice';
+import { useDispatch, useSelector } from 'react-redux';
+import { useCallback, useEffect, useRef } from 'react';
+import { Link } from 'react-router-dom';
+import Login from '../auth/login';
+
+export default function Home() {
+    const dispatch = useDispatch();
+    const { posts, loading, hasMore } = useSelector((state) => state.post);
+    const { isAuthenticated } = useSelector((state) => state.auth);
+    const observer = useRef();
+
+    const lastPostElementRef = useCallback(node => {
+        if (loading) return;
+        if (observer.current) observer.current.disconnect();
+        
+        observer.current = new IntersectionObserver(entries => {
+            if (entries[0].isIntersecting && hasMore) {
+                dispatch(getPostsBefore());
+            }
+        });
+        
+        if (node) observer.current.observe(node);
+    }, [loading, hasMore, dispatch]);
+
+    useEffect(() => {
+        dispatch(getPostsBefore());
+        
+        return () => {
+            if (observer.current) {
+                observer.current.disconnect();
+            }
+        };
+    }, [dispatch]);
+
+    useEffect(() => {
+        return () => {
+            dispatch(clearStatus());
+        };
+    }, [dispatch]);
+
+    return (
+        <>
+        {isAuthenticated ? (
+            <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 mt-4">
+                <div className="col-span-2">
+                    <>
+                        <PostForm />
+                        <div className="divide-y divide-gray-200">
+                            {posts.map((post, index) => (
+                                <div
+
+                                    key={`${post._id}-${post.createdAt}`}
+                                    ref={index === posts.length - 1 ? lastPostElementRef : null}
+                                >
+                                    <PostCard post={post} />
+                                </div>
+                            ))}
+                            {loading && (
+                                <div className="p-4 text-center text-gray-500">
+                                    Chargement...
+                                </div>
+                            )}
+                        </div>
+                    </>
+                </div>
+                {isAuthenticated && <SidebarRight />}
+            </div>
+        ) : (
+            <Login />
+        )}
+
+        </>
+
+    );
+}
